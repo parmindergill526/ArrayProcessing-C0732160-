bf8f19bd490465e677c3f3985a79dda2892265b6
parmindergill526 <41070943+parmindergill526@users.noreply.github.com>
2019-04-19 22:58:50 -0400

    longest word

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,30 +10,19 @@ namespace Assignment5Parminder
     {
         static void Main(string[] args)
         {
-            int[] inputArray = new int[10] { 3, 11, 9, 11, 4, 11, 5, 9, 2, 1 };
-            ArrayProcessing(inputArray);
-        }
-
-        public static void ArrayProcessing(int[] newArray)
-        {
-            int[] newArray2 = newArray;
-            for(int i = 0; i < newArray2.Length; i++)
-            {
-                int counter = 0;
-                
-                for(int j = 0; j<newArray2.Length; j++)
-                {
-                    if (newArray2[j] == newArray2[i])
-                    {
-                        counter++;
-                        if (counter == 3)
-                        {
-                            Console.WriteLine("Number that is repeated three time is"+newArray2[i]);
-                        }
-                    }
-                }
-            }
-
-        }
-    }
+			string line = "Ada Lovelace wrote the first algorithm designed for processing by an Analytical Engine.";
+			string[] words = line.split(' ');
+			string word = "";
+			int cntr = 0;
+			foreach(string sword in words){
+				if(s.Length > cntr)
+				{
+					word =sword;
+					cntr = sword.length;
+				}
+			}
+			
+			Console.WriteLine("the longest word is"+word);
+		}
+	}
 }
