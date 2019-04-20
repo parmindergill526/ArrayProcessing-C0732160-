a7a03fd3387460debc18cad5a8ebc33c7828cdd8
parmindergill526 <41070943+parmindergill526@users.noreply.github.com>
2019-04-19 22:56:00 -0400

    Repeated Number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Assignment5Parminder
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] inputArray = new int[10] { 3, 11, 9, 11, 4, 11, 5, 9, 2, 1 };
+            ArrayProcessing(inputArray);
+        }
+
+        public static void ArrayProcessing(int[] newArray)
+        {
+            int[] newArray2 = newArray;
+            for(int i = 0; i < newArray2.Length; i++)
+            {
+                int counter = 0;
+                
+                for(int j = 0; j<newArray2.Length; j++)
+                {
+                    if (newArray2[j] == newArray2[i])
+                    {
+                        counter++;
+                        if (counter == 3)
+                        {
+                            Console.WriteLine("Number that is repeated three time is"+newArray2[i]);
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
